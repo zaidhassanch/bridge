2ab23639d4f67d50e5189992212aa8a737009716
zaidhassanch <zaidhassanch@gmail.com>
2021-10-29 16:57:16 +0500

    Cheese ready to run

---
+++ fairseq/models/camembert/__init__.py
@@ -0,0 +1,10 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from .hub_interface import *
+from .model import *
+from .enc_dec import *
+from .model_gottbert import *
+from .model_xlmr import *
